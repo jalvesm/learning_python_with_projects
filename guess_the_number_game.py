a12bfb3dbbbb5dbae77b073253ae5ec0cb782937
Joana Morais <92741380+jalvesm@users.noreply.github.com>
2022-07-31 13:50:12 -0300

    Improvements for cleaning the code!
    - removed the 'while' loop;
    - added 'for' loop for convenience;
    - removed unnecessary variables.

--- guess_the_number_game.py
+++ guess_the_number_game.py
@@ -1,15 +1,11 @@
-
-
 print("**********************************")
 print("\tTHE GUESSING GAME")
 print("**********************************")
 
 number = 42     #Answer to the Ultimate Question of Life :)
 total_of_attempts = 3
-round = 1       #starts at round 1
-user_guess = 0
 
-while (round <= total_of_attempts):
+for round in range (1, total_of_attempts + 1):
     print("\n\nAttempt {} out of {}".format(round, total_of_attempts))
     user_guess_str = input("Inform your guess: ")
     user_guess = int(user_guess_str)
@@ -26,7 +22,5 @@ while (round <= total_of_attempts):
             print("Ohhh, not this time. Your guess was smaller than the actual number!")
         elif (guess_was_bigger):
             print("Ohhh, not this time. Your guess was bigger than the actual number!")
-    
-    round = round + 1
 
 print("End of game!")
