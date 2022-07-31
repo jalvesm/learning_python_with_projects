250090a3017a2ada662419d1f25cc640ce66b54f
Joana Morais <92741380+jalvesm@users.noreply.github.com>
2022-07-31 15:21:56 -0300

    improving the game
    - Generating the secret number as random;
    - Setting levels to the game;
    - Removing unnecessary variables.

--- guess_the_number_game.py
+++ guess_the_number_game.py
@@ -1,18 +1,30 @@
+import random
+
 print("**********************************")
 print("\tTHE GUESSING GAME")
 print("**********************************")
 
-number = 42     #Answer to the Ultimate Question of Life :)
-total_of_attempts = 3
+secret_number = random.randrange(1,101)
 
+print("Selec the level of difficulty:")
+print("(1) Easy\t (2) Medium\t (3) Hard")
+user_choice = int(input("Your choice: "))
+
+if (user_choice == 1):
+    total_of_attempts = 25
+elif (user_choice == 2):
+    total_of_attempts = 15
+else:
+    total_of_attempts = 5
+    
+    
 for round in range (1, total_of_attempts + 1):
     print("\n\nAttempt {} out of {}".format(round, total_of_attempts))
-    user_guess_str = input("Inform your guess: ")
-    user_guess = int(user_guess_str)
+    user_guess = int(input("Inform your guess: "))
 
-    guess_was_smaller = number > user_guess
-    guess_was_equal = number == user_guess
-    guess_was_bigger = number < user_guess
+    guess_was_smaller = secret_number > user_guess
+    guess_was_equal = secret_number == user_guess
+    guess_was_bigger = secret_number < user_guess
 
     if (guess_was_equal):
         print("YEAH, congrats! Right on the spot!\n")
